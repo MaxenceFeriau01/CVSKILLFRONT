b706f5271c33bec9ead78470dbac1870626b0926
Marie-Laure CASSIAU DIT LACU <marie-laure.cassiau@modiscloud.net>
2021-10-07 15:32:14 +0200

    mise en place partielle

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+
+export class User {
+
+    email!: string;
+    password!: string;
+    roles!: Array<string>;    
+    department!: string;
+    firstName!: string;
+    id?	: number ;
+    lastName!: string;
+
+    constructor(){}
+}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ComponentsModule } from '../components/components.module';
+
+
+@NgModule({
+    declarations: [
+      
+    ],
+    imports: [
+     ComponentsModule,
+    ],
+    providers: [],
+  
+  })
+  export class PagesModule { }
+  
