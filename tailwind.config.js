7a7bed955ed66efb7e85cd265672edbed111fb5d
Adrien HOLVOET <adrien.holvoet@modiscloud.net>
2022-03-22 14:42:03 +0100

    add responsivity for filter in main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,27 @@
 module.exports = {
-  important: true,
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    screens: {
-      md: '768px',
-      lg: '1024px',
-      xl: '1440px',
-    },
-    colors: {
-      'primary': '#2daf8e',
-      'secondary': '#d3d61f',
-      'gray': '#e4e2e0',
-      'warning': '#ff9966',
-      'white' :'white'
-      
-    },
-    listStyleType: {
-      none: 'none',
-      disc: 'disc',
-      decimal: 'decimal',
-      square: 'square',
-      roman: 'upper-roman',
-    }
-  }
-,
-  plugins: [],
+	important: true,
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		screens: {
+			tablet: "768px",
+			laptop: "1024px",
+			desktop: "1440px",
+		},
+		colors: {
+			primary: "var(--color-primary)",
+			secondary: "var(--color-secondary)",
+			gray: "var(--color-gray)",
+			warning: "var(--color-warning)",
+			white: "var(--color-white)",
+		},
+		listStyleType: {
+			none: "none",
+			disc: "disc",
+			decimal: "decimal",
+			square: "square",
+			roman: "upper-roman",
+		},
+	},
+
+	plugins: [],
 }
