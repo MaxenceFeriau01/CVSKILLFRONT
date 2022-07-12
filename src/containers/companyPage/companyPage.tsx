c66f07d8643ec266e7961861f59eae196cca5a8a
Adrien HOLVOET <adrien.holvoet@modiscloud.net>
2022-07-12 16:42:07 +0200

    #22430 remove isPaid filter

--- src/containers/companyPage/companyPage.tsx
+++ src/containers/companyPage/companyPage.tsx
@@ -1,5 +1,4 @@
-import { FormGroup, FormControlLabel, Checkbox } from "@mui/material"
-import { useRef, useState, useContext, ChangeEvent } from "react"
+import { useContext, useRef, useState } from "react"
 import { useInfiniteQuery, useQuery } from "react-query"
 import { useNavigate } from "react-router-dom"
 import Swal from "sweetalert2"
@@ -9,8 +8,8 @@ import Company from "../../api/models/company"
 import ReactSelectOption from "../../api/models/reactSelectOption"
 
 import activityService from "../../api/services/activityService"
-import internStatusService from "../../api/services/internStatusService"
 import companyService from "../../api/services/companyService"
+import internStatusService from "../../api/services/internStatusService"
 import CustomSelect from "../../components/inputs/customSelect"
 import CompanyTile from "./companyTile"
 import { PAGE, SIZE } from "./constants"
@@ -22,21 +21,19 @@ function CompanyPage() {
 	const canFetch = useRef(true)
 	const [filter, setFilter] = useState<number[] | null | string[]>(null)
 	const [status, setStatus] = useState<number | null | string>(null)
-	const [isPaid, setIsPaid] = useState<boolean>(false)
 
 	const { user } = useContext(UserContext)
 	const navigate = useNavigate()
 	const [selectedCompany, setSelectedCompany] = useState<Company | null>(null)
 
 	const companies = useInfiniteQuery(
-		["companies", filter, status, isPaid],
+		["companies", filter, status],
 		({ pageParam = PAGE }) =>
 			companyService.getAllPaginated({
 				page: pageParam,
 				size: SIZE,
 				activities: filter?.join(","),
 				statusId: status,
-				isPaidAndLongTermInternship: isPaid === false ? null : isPaid,
 			}),
 		{
 			getNextPageParam: data => {
@@ -68,13 +65,6 @@ function CompanyPage() {
 	function selectHandleTraineesChange(option: ReactSelectOption) {
 		setSelectedCompany(null)
 		setStatus(option === null ? null : option.value)
-	}
-
-	const selectIsPaidAndLongTermInternship = (
-		event: ChangeEvent<HTMLInputElement>
-	) => {
-		setSelectedCompany(null)
-		setIsPaid(event.target.checked)
 	}
 
 	function handleScroll(e: any) {
@@ -138,22 +128,6 @@ function CompanyPage() {
 						isSearchable
 						name="selectTrainees"
 					/>
-					<FormGroup>
-						<FormControlLabel
-							control={
-								<Checkbox
-									checked={isPaid}
-									onChange={selectIsPaidAndLongTermInternship}
-									sx={{
-										"& .MuiSvgIcon-root": { fontSize: 28 },
-									}}
-									inputProps={{ "aria-label": "controlled" }}
-								/>
-							}
-							label="Uniquement les stages de longue durée"
-							name="selectIsPaidAndLongTermInternship"
-						/>
-					</FormGroup>
 				</header>
 			) : (
 				""
