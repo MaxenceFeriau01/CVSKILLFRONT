312733ec99725e191347dfc04829c0009e17df6e
remy letailleur <remy.letailleur@modiscloud.net>
2023-08-17 11:31:49 +0200

    mise en place du filtre status

--- src/containers/statistics/statisticsIndividualPage/statisticsIndividualPage.component.tsx
+++ src/containers/statistics/statisticsIndividualPage/statisticsIndividualPage.component.tsx
@@ -14,106 +14,121 @@ function StatisticsIndividualPage() {
 	const [searchName, setSearchName] = useState<string>("")
 	const [searchFirstName, setSearchFirstName] = useState<string>("")
 	const [searchPostalCode, setSearchPostalCode] = useState<any>("")
+	const [searchStatus, setSearchStatus] = useState<any>("")
 	const { individualStatsQuery } = useStatisticsIndividualPage()
+
 	const onChangeName = (evt: any) => {
 		evt.preventDefault()
 		setSearchName(evt.target.value)
-		console.log(evt.target.value)
-		console.log(individualStatsQuery?.data)
 	}
 	const onChangeFirstName = (evt: any) => {
 		evt.preventDefault()
 		setSearchFirstName(evt.target.value)
-		console.log("Search First Name:", evt.target.value)
-		console.log(filteredRows)
 	}
 	const onChangePostalCode = (evt: any) => {
 		evt.preventDefault()
 		setSearchPostalCode(evt.target.value)
-		console.log("Search First Name:", evt.target.value)
+	}
+	const onChangeStatus = (evt: any) => {
+		evt.preventDefault()
+		setSearchStatus(evt.target.value)
+		console.log("Search status:", evt.target.value)
 		console.log(filteredRows)
 	}
-/* eslint-disable */
-	// prettier-ignore
-	let filteredRows : any = individualStatsQuery?.data
+
+	let filteredRows: any = individualStatsQuery?.data
 
 	if (searchFirstName) {
 		filteredRows = individualStatsQuery?.data?.filter(row =>
-			row.firstName
-					.toLowerCase()
-					.includes(
-						searchFirstName.toLowerCase())
+			row.firstName.toLowerCase().includes(searchFirstName.toLowerCase())
 		)
 	}
 	if (searchName) {
 		filteredRows = individualStatsQuery?.data?.filter(row =>
-			row.name
-				.toLowerCase()
-				.includes(
-					searchName.toLowerCase())
+			row.name.toLowerCase().includes(searchName.toLowerCase())
 		)
 	}
 	if (searchPostalCode) {
-		const postalCodeToNumber = parseInt(searchPostalCode)
-		filteredRows = individualStatsQuery?.data?.filter(row =>
-			row.postalCode === postalCodeToNumber
+		const postalCodeToNumber = parseInt(searchPostalCode, 10)
+		filteredRows = individualStatsQuery?.data?.filter(
+			row => row.postalCode === postalCodeToNumber
 		)
 	}
-		
+	if (searchStatus) {
+		filteredRows = individualStatsQuery?.data?.filter(row =>
+			row.internStatus.name
+				.toLowerCase()
+				.includes(searchStatus.toLowerCase())
+		)
+	}
 
-		/* eslint-enable */
 	return (
 		<section className="page">
-			<header className="user-page-header">
-				<TextField
-					id="searchUserName"
-					label="Rechercher par nom"
-					value={searchName}
-					onChange={onChangeName}
-					InputProps={{
-						endAdornment: (
-							<InputAdornment position="start">
-								<SearchIcon />
-							</InputAdornment>
-						),
-					}}
-				/>
-				<TextField
-					id="searchUserFirstName"
-					label="Rechercher par prénom"
-					value={searchFirstName}
-					onChange={onChangeFirstName}
-					InputProps={{
-						endAdornment: (
-							<InputAdornment position="start">
-								<SearchIcon />
-							</InputAdornment>
-						),
-					}}
-				/>
-				<TextField
-					id="searchUserPostalCode"
-					label="Rechercher par code postale"
-					value={searchPostalCode}
-					onChange={onChangePostalCode}
-					InputProps={{
-						endAdornment: (
-							<InputAdornment position="start">
-								<SearchIcon />
-							</InputAdornment>
-						),
-					}}
-				/>
-			</header>
-			<div className="content h-full p-3">
-				<DataGrid
-					columns={TABLE_COLUMNS}
-					rows={filteredRows || []}
-					loading={individualStatsQuery?.isLoading}
-					pagination
-					localeText={LOCALE_LANG}
-					rowsPerPageOptions={PAGE_SIZE_OPTIONS}
-				/>
+			<div className="content user-content">
+				<header className="user-page-header">
+					<TextField
+						id="searchUserName"
+						label="Rechercher par nom"
+						value={searchName}
+						onChange={onChangeName}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="start">
+									<SearchIcon />
+								</InputAdornment>
+							),
+						}}
+					/>
+					<TextField
+						id="searchUserFirstName"
+						label="Rechercher par prénom"
+						value={searchFirstName}
+						onChange={onChangeFirstName}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="start">
+									<SearchIcon />
+								</InputAdornment>
+							),
+						}}
+					/>
+					<TextField
+						id="searchUserPostalCode"
+						label="Rechercher par code postale"
+						value={searchPostalCode}
+						onChange={onChangePostalCode}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="start">
+									<SearchIcon />
+								</InputAdornment>
+							),
+						}}
+					/>
+					<TextField
+						id="searchUserStatus"
+						label="Rechercher par status"
+						value={searchStatus}
+						onChange={onChangeStatus}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="start">
+									<SearchIcon />
+								</InputAdornment>
+							),
+						}}
+					/>
+				</header>
+				<div className="content h-full p-3">
+					<DataGrid
+						columns={TABLE_COLUMNS}
+						rows={filteredRows || []}
+						loading={individualStatsQuery?.isLoading}
+						pagination
+						localeText={LOCALE_LANG}
+						rowsPerPageOptions={PAGE_SIZE_OPTIONS}
+					/>
+				</div>
 			</div>
 		</section>
 	)
