28336168a2e48049ea8d73e89c720c96c9a2c4f2
remy letailleur <remy.letailleur@modiscloud.net>
2023-08-18 16:08:21 +0200

    in progres : pagination

--- src/containers/statistics/statisticsIndividualPage/statisticsIndividualPage.component.tsx
+++ src/containers/statistics/statisticsIndividualPage/statisticsIndividualPage.component.tsx
@@ -1,22 +1,29 @@
+import React, { useState } from "react"
 import InputAdornment from "@mui/material/InputAdornment/InputAdornment"
 import TextField from "@mui/material/TextField/TextField"
 import SearchIcon from "@mui/icons-material/Search"
-import { DataGrid } from "@mui/x-data-grid"
-import { useState } from "react"
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid"
+import { FileDownload } from "@mui/icons-material"
+import * as XLSX from "xlsx"
 import {
 	LOCALE_LANG,
+	PAGE_SIZE_DEFAULT,
 	PAGE_SIZE_OPTIONS,
 	TABLE_COLUMNS,
 } from "./statisticsIndividualPage.constant"
 import useStatisticsIndividualPage from "./statisticsIndividualPage.hook"
+
+import user from "../../../contexts/user"
 
 function StatisticsIndividualPage() {
 	const [searchName, setSearchName] = useState<string>("")
 	const [searchFirstName, setSearchFirstName] = useState<string>("")
 	const [searchPostalCode, setSearchPostalCode] = useState<any>("")
 	const [searchStatus, setSearchStatus] = useState<any>("")
-	const { individualStatsQuery } = useStatisticsIndividualPage()
-
+	const [currentPage, setCurrentPage] = useState(1)
+	const [pageSize, setPageSize] = useState<number>(PAGE_SIZE_DEFAULT)
+	const { individualStatsQuery, fetchIndividualStats } =
+		useStatisticsIndividualPage(currentPage, pageSize)
 	const onChangeName = (evt: any) => {
 		evt.preventDefault()
 		setSearchName(evt.target.value)
@@ -34,32 +41,75 @@ function StatisticsIndividualPage() {
 		setSearchStatus(evt.target.value)
 	}
 
-	let filteredRows: any = individualStatsQuery?.data
+	let filteredRows: any = individualStatsQuery?.data?.content
+
+	console.log("filteredRows")
+	console.log(individualStatsQuery)
 
 	if (searchFirstName) {
-		filteredRows = individualStatsQuery?.data?.filter(row =>
-			row.firstName.toLowerCase().includes(searchFirstName.toLowerCase())
+		filteredRows = individualStatsQuery?.data.filter(
+			(row: { firstName: string }) =>
+				row.firstName
+					.toLowerCase()
+					.includes(searchFirstName.toLowerCase())
 		)
 	}
 	if (searchName) {
-		filteredRows = individualStatsQuery?.data?.filter(row =>
-			row.name.toLowerCase().includes(searchName.toLowerCase())
+		filteredRows = individualStatsQuery?.data.filter(
+			(row: { name: string }) =>
+				row.name.toLowerCase().includes(searchName.toLowerCase())
 		)
 	}
 	if (searchPostalCode) {
 		const postalCodeToNumber = parseInt(searchPostalCode, 10)
-		filteredRows = individualStatsQuery?.data?.filter(
-			row => row.postalCode === postalCodeToNumber
+		filteredRows = individualStatsQuery?.data.filter(
+			(row: { postalCode: number }) =>
+				row.postalCode === postalCodeToNumber
 		)
 	}
 	if (searchStatus) {
-		filteredRows = individualStatsQuery?.data?.filter(row =>
-			row.internStatus.name
-				.toLowerCase()
-				.includes(searchStatus.toLowerCase())
+		filteredRows = individualStatsQuery?.data.filter(
+			(row: { internStatus: { name: string } }) =>
+				row.internStatus.name
+					.toLowerCase()
+					.includes(searchStatus.toLowerCase())
 		)
 	}
 
+	const handleExport = (data: any[]) => () => {
+		const filteredData = data.map(item => {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const { id, ...rest } = item
+			return rest
+		})
+		const customHeader = [
+			"Date d'inscription",
+			"Civilité",
+			"Nom",
+			"Prénom",
+			"Téléphone",
+			"Email",
+			"Code postal",
+			"Statut",
+			"durée de stage",
+			"Niveau de diplome",
+			"Nombre de mise à jour du profil",
+		]
+		const wb = XLSX.utils.book_new()
+		const ws = XLSX.utils.json_to_sheet([], { header: customHeader })
+		XLSX.utils.sheet_add_json(ws, filteredData, {
+			skipHeader: true,
+			origin: "A2",
+		})
+
+		XLSX.utils.book_append_sheet(wb, ws, "Sheet1")
+
+		XLSX.writeFile(wb, "export_statistique_individuel.xlsx")
+	}
+	const handlePageChange = (newPage: number) => {
+		fetchIndividualStats(newPage, pageSize)
+		setCurrentPage(newPage)
+	}
 	return (
 		<section className="page">
 			<div className="content user-content">
@@ -92,7 +142,7 @@ function StatisticsIndividualPage() {
 					/>
 					<TextField
 						id="searchUserPostalCode"
-						label="Rechercher par code postale"
+						label="Rechercher par code postal"
 						value={searchPostalCode}
 						onChange={onChangePostalCode}
 						InputProps={{
@@ -116,13 +166,32 @@ function StatisticsIndividualPage() {
 							),
 						}}
 					/>
+					<GridActionsCellItem
+						icon={<FileDownload color="primary" />}
+						label="Exporter"
+						onClick={handleExport(filteredRows)}
+						title="Exporter"
+					/>
 				</header>
 				<div className="content h-full p-3">
 					<DataGrid
 						columns={TABLE_COLUMNS}
 						rows={filteredRows || []}
+						pageSize={pageSize}
+						// pageSize={individualStatsQuery?.data.size}
 						loading={individualStatsQuery?.isLoading}
+						rowCount={
+							individualStatsQuery?.data?.totalElements || 0
+						}
+						onPageChange={newPage => {
+							handlePageChange(newPage)
+						}}
+						onPageSizeChange={newPageSize => {
+							setPageSize(newPageSize) // Update the page size state
+							setCurrentPage(1) // Reset the current page to 1 when changing page size
+						}}
 						pagination
+						paginationMode="server"
 						localeText={LOCALE_LANG}
 						rowsPerPageOptions={PAGE_SIZE_OPTIONS}
 					/>
--- src/containers/statistics/statisticsIndividualPage/statisticsIndividualPage.hook.tsx
+++ src/containers/statistics/statisticsIndividualPage/statisticsIndividualPage.hook.tsx
@@ -1,14 +1,43 @@
 import { useQuery } from "react-query"
 import userService from "../../../api/services/userService"
 
-function useStatisticsIndividualPage() {
-	const individualStatsQuery = useQuery(["user-stats"], () =>
-		userService.getUserStats()
+function useStatisticsIndividualPage(initialPage = 1, initialPageSize = 10) {
+	const individualStatsQuery = useQuery(
+		["user-stats", initialPage, initialPageSize],
+		() => {
+			console.log(
+				"Fetching data with page:",
+				initialPage,
+				"and pageSize:",
+				initialPageSize
+			)
+			return userService.getAllPaginated({
+				page: initialPage,
+				size: initialPageSize,
+			})
+		}
 	)
+
+	// Log the query status and data when they change
+	console.log("Query status:", individualStatsQuery.status)
+	console.log("Query data:", individualStatsQuery.data)
 
 	return {
 		individualStatsQuery,
+		fetchIndividualStats: (
+			page: unknown | string,
+			pageSize: unknown | string
+		) => {
+			console.log(
+				"Fetching data with page:",
+				page,
+				"and pageSize:",
+				pageSize
+			)
+			individualStatsQuery.refetch({
+				queryKey: ["user-stats", page, pageSize],
+			})
+		},
 	}
 }
-
 export default useStatisticsIndividualPage
