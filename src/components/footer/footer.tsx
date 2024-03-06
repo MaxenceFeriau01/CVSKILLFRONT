45c5126410b142af0c702d135ee5b05350995388
Osama GHONEIM <osama-ahmed-kadry.ghoneim@akkodis.com>
2024-03-06 13:52:06 +0000

    Merged PR 15352: Add 2 more static messages in the pastille down the page
    Un interval de cinq secondes a été créé afin qu'un nouveau message soit afficher dans la pastille
    
    Related work items: #39491

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react"
+import { MESSAGES } from "./constant"
+
 function Footer() {
+	const [message, setMessage] = useState<{
+		label: string
+		link: string
+		isMail: boolean
+	}>(MESSAGES[0])
+
+	useEffect(() => {
+		let index: number = 0
+
+		const displayMessageSchedular = () => {
+			setMessage(MESSAGES[index])
+			index = (index + 1) % MESSAGES.length
+		}
+
+		const intervalId = setInterval(displayMessageSchedular, 5000)
+
+		return () => {
+			clearInterval(intervalId)
+		}
+	}, [])
+
 	return (
 		<div className="footer">
 			<p>
-				Besoin d’aide, d’une info ? Envoyez vos questions sur l’adresse
-				mail <a href="mailto:dkstages@eedk.fr">dkstages@eedk.fr</a>
+				{message.label}&nbsp;
+				{message.isMail ? (
+					<a href={`mailto:${message.link}`}>{message.link}</a>
+				) : (
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href={message.link}
+					>
+						{message.link}
+					</a>
+				)}
 			</p>
 		</div>
 	)
